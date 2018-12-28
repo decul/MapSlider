4f7ad995284575a8a0989125f7e42901f5cb079f
tdec <tomdec@o2.pl>
2018-12-28 17:06:05 +0100

    Add map selectors

--- src/mapslider.js
+++ src/mapslider.js
@@ -1,7 +1,12 @@
+var configs = [];
+configs.push({ name:"1888 mapa 2k", fileName:"1888 map 2k.png", w:2462, h:1758, x:1467, y:926, z:0.9238, r:0 });
+configs.push({ name:"1944 mapa 10k", fileName:"1944 map 10k.jpg", w:10187, h:9666, x:5738, y:4514, z:0.2, r:0 });
+configs.push({ name:"2012 mapa 3k", fileName:"2012 map 3k.png", w:3072, h:2560, x:1552, y:1309, z:0.826, r:2.42 });
 
-var sync = {}
-sync["m1944m10k"] = { w:10187, h:9666, x:5738, y:4514, z:0.2, r:0 };
-sync["m2012m3k"] = { w:3072, h:2560, x:1551, y:1309, z:0.826, r:2.42 };
+
+var leftMapIndex = 0;
+var rightMapIndex = 1;
+
 
 const zoomDelta = 1.42;
 const translationDelta = 100;
@@ -10,7 +15,10 @@ var zoomRatio = 1.0;
 var translationX = 0.0;
 var translationY = 0.0;
 
+
+
 var scrollZoom = function(e) {
+    e.preventDefault();
     if(e.originalEvent.wheelDelta /120 > 0) {
         zoomRatio *= zoomDelta;
     }
@@ -21,6 +29,7 @@ var scrollZoom = function(e) {
 }
 
 var keyboard = function(e) {
+    e.preventDefault();
     switch(e.key) {
         case 'ArrowUp':
             translationY += translationDelta / zoomRatio;
@@ -45,18 +54,13 @@ var keyboard = function(e) {
         case '-':
             zoomRatio /= zoomDelta;
             break;
-
-        default:
-            console.log(e);
-            break;
     }
     transform();
 }
 
 var transform = function() {
-    transformSide('left', sync["m1944m10k"]);
-    transformSide('right', sync["m2012m3k"]);
-    
+    transformSide('left', configs[leftMapIndex]);
+    transformSide('right', configs[rightMapIndex]);
 }
 
 var transformSide = function(side, conf) {
@@ -77,17 +81,39 @@ var transformSide = function(side, conf) {
 }
 
 var rotateVectors = function() {
-    for(var conf in Object.values(sync)){
-        //var conf = sync[key];
+    $.each(configs, function(conf) {
         var angle = conf.r * (Math.PI/180);
         var x = conf.w / 2 - conf.x;
         var y = conf.h / 2 - conf.y;
-        conf.x = x * Math.cos(angle) - y * Math.sin(angle);
-        conf.y = x * Math.sin(angle) + y * Math.cos(angle);
-    }
+        conf.x = conf.w / 2 - (x * Math.cos(angle) - y * Math.sin(angle));
+        conf.y = conf.h / 2 - (x * Math.sin(angle) + y * Math.cos(angle));
+    });
+}
+
+var fillComboboxes = function() {
+    var $comboboxes = $('.combobox');
+    $.each(configs, function(index, conf) {
+        $comboboxes.append($("<option />").val(index).text(conf.name));
+    });
+    $('#left-combobox').val(leftMapIndex);
+    $('#right-combobox').val(rightMapIndex);
+    comboboxChanged();
+}
+
+var comboboxChanged = function() {
+    leftMapIndex = $('#left-combobox').val();
+    rightMapIndex = $('#right-combobox').val();
+
+    $('.left.image img').attr("src", "src/img/" + configs[leftMapIndex].fileName);
+    $('.right.image img').attr("src", "src/img/" + configs[rightMapIndex].fileName);
+
+    transform();
 }
 
 var initMapSlider = function() {
+    $('.combobox').change(comboboxChanged);
+    fillComboboxes();
+
     $('.slider').on('mousewheel', scrollZoom);
     $(document).keydown(keyboard);
 
